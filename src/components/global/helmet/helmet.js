589848638a00eac2b21bf3c332428fc25a89c292
dylandoamaral <do.amaral.dylan@gmail.com>
2020-05-03 21:18:38 +0200

    💻 fix: schema scripts

--- src/components/global/helmet/helmet.js
+++ src/components/global/helmet/helmet.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-export default (props) => (
+export default props => (
     <Helmet title="Dylan Do Amaral" defer={false}>
+        <script type="application/ld+json">
+            {JSON.stringify({
+                "@content": "https://www.dylandoamaral.me/",
+                "@type": "WebPage",
+                headline: "Dylan Do Amaral",
+            })}
+        </script>
         {props.children}
         <meta name="author" content="Dylan Do Amaral" />
         <meta property="og:type" content="website" />
@@ -16,7 +23,6 @@ export default (props) => (
             name="keywords"
             content="dylan, dylan do amaral, do amaral, engineer, ingénieur, big data, ingénieur data, website, gatsby, date engineer, ingénieur de la donnée"
         />
-
         <meta
             name="image"
             property="og:image"
@@ -27,7 +33,6 @@ export default (props) => (
             property="og:url"
             content="https://www.dylandoamaral.me/"
         />
-
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Dylan Do Amaral" />
         <meta
@@ -38,32 +43,7 @@ export default (props) => (
             name="twitter:image"
             content="http://www.dylandoamaral.me/index/taiwan.jpg"
         />
-        
         <meta http-equiv="content-language" content="fr-fr" />
         <html lang="fr" />
-
-        <script type="application/ld+json">
-                {{
-                    "@context": "https://schema.org",
-                    "@type": "Organization",
-                    url: "https://www.dylandoamaral.me/",
-                    givenName: "Dylan",
-                    familyName: "Do Amaral",
-                    image: "https://www.dylandoamaral.me/index/taiwan",
-                    jobTitle: "Data Engineer",
-                    gender: "https://schema.org/Male",
-                    hasOccupation: {
-                        "@type": "Occupation",
-                        "educationRequirements": "bac +5",
-                        "experienceRequirements": "data engineer degree",
-                        "occupationLocation": "paris",
-                    },
-                    contactPoint: {
-                        "@type": "ContactPoint",
-                        telephone: "+336-51-07-66-14",
-                        contactType: "Numéro personnel",
-                    },
-                }}
-            </script>
     </Helmet>
 );
