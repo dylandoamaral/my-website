b7c3de2eebe98db9a7e97db575a2a7692b719bb9
dylandoamaral <do.amaral.dylan@gmail.com>
2020-05-03 17:13:21 +0200

    💻 update: gatsby packages

--- src/components/global/layout/layout.js
+++ src/components/global/layout/layout.js
@@ -1,7 +1,5 @@
 import React from "react"
 
-import PageTransition from "gatsby-plugin-page-transitions"
-
 import Header from "../header/header"
 import Footer from "../footer/footer"
 
@@ -10,7 +8,7 @@ import { Layout } from "./layout.style"
 export default (props) => (
     <Layout>
         <Header page={props.page}/>
-        <PageTransition>{props.children}</PageTransition>
+        {props.children}
         <Footer />
     </Layout>
 )
