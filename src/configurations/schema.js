52d039234b7e0b4f6be58b38e6ea84557c47d722
dylandoamaral <do.amaral.dylan@gmail.com>
2020-05-03 23:25:40 +0200

    👀 delete: meSchema occupation

--- src/configurations/schema.js
+++ src/configurations/schema.js
@@ -13,19 +13,9 @@ export const meSchema = {
     image: "https://www.dylandoamaral.me/index/taiwan",
     jobTitle: "Data Engineer",
     gender: "https://schema.org/Male",
-    hasOccupation: {
-        "@type": "Occupation",
-        educationRequirements: "bac +5",
-        experienceRequirements: "data engineer degree",
-        occupationLocation: "paris",
-        estimatedSalary: "-1",
-        mainEntityOfPage: "https://www.dylandoamaral.me/",
-        description: "It is me"
-    },
     contactPoint: {
         "@type": "ContactPoint",
         telephone: "+336-51-07-66-14",
         contactType: "Numéro personnel",
     },
-    name: "Dylan DO AMARAL",
 };
